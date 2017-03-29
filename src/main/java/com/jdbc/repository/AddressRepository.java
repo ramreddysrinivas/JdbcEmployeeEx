95771d55d0f46da97b9b7c2ee13781f83f3f7a68
ramreddysrinivas <ramreddysrinivas532@gmail.com>
2017-03-29 13:59:53 +0530

    code added for Addressrepository with crud operations and added Select query implimentation for employeerepository

---
+++ src/main/java/com/jdbc/repository/AddressRepository.java
@@ -0,0 +1,261 @@
+package com.jdbc.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jdbc.domain.Address;
+
+
+public class AddressRepository {
+	
+	public static void main(String[] args) {
+		
+		AddressRepository address = new AddressRepository();
+		//address.insertWithStatement();
+		//List<Address> empList1		= address.retrieveEmployeesWithStmt();
+		//address.insertWithPreparedStatement();
+		//List<Address> empList1		= address.retrieveEmployeesWithPstmt();
+		Address a1= address.getAddressByZipcodeWithPstmt("500072");
+		System.out.println(a1.getZipcode() + "," + a1.getArea() + ", " + a1.getStreet() + "," + a1.getDistrict());
+	//Address a2	=  address.getAddressByZipcodeWithStatement("503225");
+	//System.out.println(a2.getZipcode() + " , " + a2.getStreet() + ", " + a2.getArea() + ", " + a2.getDistrict());
+		
+		//address.printEmployees(empList1);
+
+	}
+
+ private void printEmployees(List<Address> empList1) {
+		for (Address i : empList1) {
+			System.out.println(i.getZipcode() + "," + i.getStreet() + ", " + i.getArea() + ", " + i.getDistrict() );
+		}
+	}
+
+	private List<Address> retrieveEmployeesWithStmt() {
+		Connection con=null;
+		Statement stmt=null;
+		ResultSet rs=null;
+		List<Address> emps = new ArrayList<Address>();
+		try {
+			con = DataBaceConnectionUtil.getConnection();
+			stmt = con.createStatement();
+			String sql = "select zipcode,street,area,district from Address";
+			rs = stmt.executeQuery(sql);
+			while(rs.next()){
+				Address e = new Address();
+			String zipcode = rs.getString("zipcode");
+			String street = rs.getString("street");
+			String area = rs.getString("area");
+			String district = rs.getString("district");
+			e.setZipcode(zipcode);
+			e.setStreet(street);
+			e.setArea(area);
+			e.setDistrict(district);
+			emps.add(e);
+			}
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				DataBaceConnectionUtil.closeRs(rs);
+				DataBaceConnectionUtil.closeStmt(stmt);
+				DataBaceConnectionUtil.closeConnection(con);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return emps;
+		
+	}
+
+	private Address getAddressByZipcodeWithStatement(String zipcode) {
+		Connection  con =null;
+		Statement stmt=null;
+		ResultSet rs=null;
+		Address a = null;
+		try{
+			con=DataBaceConnectionUtil.getConnection();
+			stmt=con.createStatement();
+			String sql="select * from Address where zipcode = " + "'"  + zipcode + "'" ;
+			//select * from Address where zipcode = '503225';
+			
+			rs=stmt.executeQuery(sql);
+			
+			while(rs.next()){
+				a=new Address();
+				
+				a.setZipcode(rs.getString("zipcode"));
+				a.setStreet(rs.getString("street"));
+				a.setArea(rs.getString("area"));
+				a.setDistrict(rs.getString("district"));
+				
+				
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			try {
+				DataBaceConnectionUtil.closeStmt(stmt);
+				DataBaceConnectionUtil.closeConnection(con);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return a;
+	}
+	
+	private void insertWithPreparedStatement() {
+		Connection con =null;
+		PreparedStatement pstmt=null;
+		try {
+			con = DataBaceConnectionUtil.getConnection();
+			pstmt= con.prepareStatement("insert into Address(zipcode,street,area,district) values(?,?,?,?)");
+			 pstmt.setString(1,"500038");
+			 pstmt.setString(2,"mg colony");
+			 pstmt.setString(3,"sr nagar");
+			 pstmt.setString(4,"rangareddy");
+			 int i = pstmt.executeUpdate();
+			 System.out.println(i  +   "   rows inserted");
+		} catch (SQLException e) {
+				e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();	
+		}finally{
+			try {
+				DataBaceConnectionUtil.closePstmt(pstmt);
+				DataBaceConnectionUtil.closeConnection(con);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+	private void insertWithStatement() {
+		Connection con=null;
+		Statement stmt=null;
+		String zipcode="500072";
+		String street="viveknagar";
+		String area="kukatpally";
+		String district="medchal";
+		try {
+			con = DataBaceConnectionUtil.getConnection();
+			   stmt = con.createStatement();
+			   
+			   StringBuilder   builder = new StringBuilder("insert into Address(zipcode,street,area,district) values(" );
+			   builder.append("'").append( zipcode).append("'").append(",");
+			   builder.append("'").append( street).append("'").append(",");
+			   builder.append("'").append(area).append("'").append(",");
+			   builder.append("'").append(district).append("'").append(")");
+			  
+			  int i=stmt.executeUpdate(builder.toString());
+			  System.out.println(i  +   "   rows inserted");
+			  
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				DataBaceConnectionUtil.closeStmt(stmt);
+				DataBaceConnectionUtil.closeConnection(con);
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	private List<Address> retrieveEmployeesWithPstmt() {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		List<Address> emps = new ArrayList<Address>();
+		try {
+			con =DataBaceConnectionUtil.getConnection();
+			pstmt = con.prepareStatement("select zipcode,street,area,district from Address" );
+			
+			ResultSet rs1 = pstmt.executeQuery();
+			while(rs1.next()){
+				Address e = new Address();
+			String zipcode = rs1.getString("zipcode");
+			String street = rs1.getString("street");
+			String area = rs1.getString("area");
+			String district = rs1.getString("district");
+			e.setZipcode(zipcode);
+			e.setStreet(street);
+			e.setArea(area);
+			e.setDistrict(district);
+			emps.add(e);
+		
+			}
+			}
+			
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				DataBaceConnectionUtil.closeRs(rs);
+				DataBaceConnectionUtil.closePstmt(pstmt);
+				DataBaceConnectionUtil.closeConnection(con);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return emps;
+		
+	}
+
+	private Address getAddressByZipcodeWithPstmt(String zipcode) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		Address a = null;
+		
+		try{
+			con =DataBaceConnectionUtil.getConnection();
+			pstmt = con.prepareStatement("select * from Address where zipcode=?" );
+			pstmt.setString(1,zipcode);
+			rs=pstmt.executeQuery();
+			while(rs.next()){
+				a= new Address();
+				
+				a.setZipcode(rs.getString("zipcode"));
+				a.setStreet(rs.getString("street"));
+				a.setArea(rs.getString("area"));
+				a.setDistrict(rs.getString("district"));
+				
+				
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		finally{try {
+			DataBaceConnectionUtil.closeRs(rs);
+			DataBaceConnectionUtil.closePstmt(pstmt);
+			DataBaceConnectionUtil.closeConnection(con);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+			
+		}
+return a;
+	}
+	
+
+}
+
--- src/main/java/com/jdbc/repository/EmployeeRepository.java
+++ src/main/java/com/jdbc/repository/EmployeeRepository.java
@@ -17,12 +17,22 @@ public class EmployeeRepository {
 		
 		EmployeeRepository employeeInsert = new EmployeeRepository();
 		//employeeInsert.insertWithStatement();
+		//List<Employee> empList1		= employeeInsert.retrieveEmployeesWithStmt();
+		Employee e1 = employeeInsert.getEmployeeByEmpidWithStmt("12");
+		System.out.println( e1.getEmpid() + " , "  +e1.getEmpname() + " , "  +  e1.getEmpdesg());
+		Employee e2 = employeeInsert.getEmployeeByEmpidWithPstmt("13");
+		System.out.println( e2.getEmpid() + " , "  +e2.getEmpname() + " , "  +  e2.getEmpdesg());
+		//List<Employee> empList1		= employeeInsert.selectMulitipleFromEmployeesWithStmt();
+		//employeeInsert.updateWithStatementOneRow();
+		//employeeInsert.updateWithStatementMultipleRows();
+		//employeeInsert.deleteWithStmt();
 		//employeeInsert.insertWithPreparedStatement();
-		//List<Employee> empList1		= employeeInsert.retrieveEmployeesWithhStmt();
-		List<Employee> empList1		= employeeInsert.retrieveEmployeesWithPstmt();
-		//employeeInsert.printEmpoyees(empList1);
-		
-		employeeInsert.printEmployees(empList1);
+		//List<Employee> empList1		= employeeInsert.retrieveEmployeesWithPstmt();
+		//List<Employee> empList1		= employeeInsert.selectMulitipleFromEmployeesWithPstmt();
+		//employeeInsert.updateWithPrepareStatementOneRow();
+		//employeeInsert.updateWithPrepareStatementMultipleRows();
+		//employeeInsert.deleteWithPstmt();
+	    // employeeInsert.printEmployees(empList1);
 
 	}
 
@@ -31,8 +41,41 @@ public class EmployeeRepository {
 			System.out.println(i.getEmpid() + "," + i.getEmpname() + ", " + i.getEmpdesg());
 		}
 	}
+	private void insertWithStatement() {
+		Connection con=null;
+		Statement stmt=null;
+		String empId="13";
+		String empName="nag";
+		String desg="civil eng";
+		try {
+			con = DataBaceConnectionUtil.getConnection();
+			   stmt = con.createStatement();
+			   
+			   StringBuilder   builder = new StringBuilder("insert into Employee(empid,empname,designation) values(" );
+			   builder.append("'").append( empId).append("'").append(",");
+			   builder.append("'").append( empName).append("'").append(",");
+			   builder.append("'").append(desg).append("'").append(")");
+			  
+			  int i=stmt.executeUpdate(builder.toString());
+			  System.out.println(i  +   "   rows inserted");
+			  
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				DataBaceConnectionUtil.closeStmt(stmt);
+				DataBaceConnectionUtil.closeConnection(con);
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
 
-	private List<Employee> retrieveEmployeesWithhStmt() {
+	private List<Employee> retrieveEmployeesWithStmt() {
 		Connection con=null;
 		Statement stmt=null;
 		ResultSet rs=null;
@@ -70,6 +113,156 @@ public class EmployeeRepository {
 		return emps;
 		
 	}
+	private Employee getEmployeeByEmpidWithStmt(String empid) {
+
+		Connection con=null;
+		Statement stmt=null;
+		ResultSet rs=null;
+		Employee e=null;
+	/*List<Employee> emps = new ArrayList<Employee>();*/
+		//Employee e1 = new Employee();
+		try {
+			con = DataBaceConnectionUtil.getConnection();
+			stmt = con.createStatement();
+			String sql = "select * from Employee where empid=" + "'" + empid + "'";
+			rs = stmt.executeQuery(sql);
+			while(rs.next()){
+				e = new Employee();
+			 e.setEmpid(rs.getString("empid"));
+			 e.setEmpname( rs.getString("empName"));
+			 e.setEmpdesg(rs.getString("designation"));
+			}
+			
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}finally{
+			try {
+				DataBaceConnectionUtil.closeRs(rs);
+				DataBaceConnectionUtil.closeStmt(stmt);
+				DataBaceConnectionUtil.closeConnection(con);
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+			 
+		}
+		return e;
+		
+	}
+	
+	private List<Employee> selectMulitipleFromEmployeesWithStmt() {
+		Connection con=null;
+		Statement stmt=null;
+		ResultSet rs=null;
+		List<Employee> emps = new ArrayList<Employee>();
+		try {
+			con = DataBaceConnectionUtil.getConnection();
+			stmt = con.createStatement();
+			String sql = "select empid,empname from employee";
+			rs = stmt.executeQuery(sql);
+			while(rs.next()){
+				Employee e = new Employee();
+			String empid = rs.getString("empid");
+			String empname = rs.getString("empName");
+			//String desg = rs.getString("designation");
+			e.setEmpid(empid);
+			e.setEmpname(empname);
+			//e.setEmpdesg(desg);
+			emps.add(e);
+			}
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				DataBaceConnectionUtil.closeRs(rs);
+				DataBaceConnectionUtil.closeStmt(stmt);
+				DataBaceConnectionUtil.closeConnection(con);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return emps;
+		
+	}
+
+	private void updateWithStatementOneRow() {
+		Connection con=null;
+		Statement stmt=null;
+		try {
+			con = DataBaceConnectionUtil.getConnection();
+			   stmt = con.createStatement();
+			   String sql  = "update Employee set empname='srinu' where empid='10'" ;
+			  int i=stmt.executeUpdate(sql);
+			  System.out.println(i  +   "   rows updated ");
+			  
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				DataBaceConnectionUtil.closeStmt(stmt);
+				DataBaceConnectionUtil.closeConnection(con);
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	private void updateWithStatementMultipleRows() {
+		Connection con=null;
+		Statement stmt=null;
+		try {
+			con = DataBaceConnectionUtil.getConnection();
+			   stmt = con.createStatement();
+			   String sql  = "update Employee set empname='charan',designation='developer' where empid='10'" ;
+			  int i=stmt.executeUpdate(sql);
+			  System.out.println(i  +   "   rows updated ");
+			  
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				DataBaceConnectionUtil.closeStmt(stmt);
+				DataBaceConnectionUtil.closeConnection(con);
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}		
+	}
+
+	private void deleteWithStmt() {
+		Connection con=null;
+		Statement stmt=null;
+		try {
+			con = DataBaceConnectionUtil.getConnection();
+			   stmt = con.createStatement();
+			   String sql  = "delete from employee where empid='10'" ;
+			  int i=stmt.executeUpdate(sql);
+			  System.out.println(i  +   "   rows deleted ");
+			  
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				DataBaceConnectionUtil.closeStmt(stmt);
+				DataBaceConnectionUtil.closeConnection(con);
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	
 	private void insertWithPreparedStatement() {
 		Connection con =null;
@@ -97,43 +290,6 @@ public class EmployeeRepository {
 		
 	}
 
-	private void insertWithStatement() {
-		Connection con=null;
-		Statement stmt=null;
-		String empId="13";
-		String empName="nag";
-		String desg="civil eng";
-		try {
-			con = DataBaceConnectionUtil.getConnection();
-			   stmt = con.createStatement();
-			   
-			   StringBuilder   builder = new StringBuilder("insert into Employee(empid,empname,designation) values(" );
-			   builder.append("'").append( empId).append("'").append(",");
-			   builder.append("'").append( empName).append("'").append(",");
-			   builder.append("'").append(desg).append("'").append(")");
-			  
-			  int i=stmt.executeUpdate(builder.toString());
-			  System.out.println(i  +   "   rows inserted");
-			  
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}finally{
-			try {
-				DataBaceConnectionUtil.closeStmt(stmt);
-				DataBaceConnectionUtil.closeConnection(con);
-				
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-	}
-
-	
-
-	
 	
 	private List<Employee> retrieveEmployeesWithPstmt() {
 		Connection con=null;
@@ -144,12 +300,12 @@ public class EmployeeRepository {
 			con =DataBaceConnectionUtil.getConnection();
 			pstmt = con.prepareStatement("select empid,empname,designation from employee" );
 			
-			ResultSet rs1 = pstmt.executeQuery();
-			while(rs1.next()){
+			rs = pstmt.executeQuery();
+			while(rs.next()){
 			Employee e = new Employee();
-			String empid = rs1.getString("empid");
-			String empname = rs1.getString("empName");
-			String desg = rs1.getString("designation");
+			String empid = rs.getString("empid");
+			String empname = rs.getString("empName");
+			String desg = rs.getString("designation");
 			e.setEmpid(empid);
 			e.setEmpname(empname);
 			e.setEmpdesg(desg);
@@ -159,10 +315,8 @@ public class EmployeeRepository {
 			}
 			
 		catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}finally{
 			try {
@@ -177,5 +331,158 @@ public class EmployeeRepository {
 		return emps;
 		
 	}
+	
+	private Employee getEmployeeByEmpidWithPstmt(String empid) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		Employee e=null; 
+		try {
+			con =DataBaceConnectionUtil.getConnection();
+			pstmt = con.prepareStatement("select * from employee where empid=?" );
+			pstmt.setString(1,empid);
+			rs = pstmt.executeQuery();
+			while(rs.next()){
+				e = new Employee();
+				 e.setEmpid(rs.getString("empid"));
+				 e.setEmpname( rs.getString("empName"));
+				 e.setEmpdesg(rs.getString("designation"));
+			}
+			}
+			
+		catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}finally{
+			try {
+				
+				DataBaceConnectionUtil.closeRs(rs);
+				DataBaceConnectionUtil.closePstmt(pstmt);
+				DataBaceConnectionUtil.closeConnection(con);
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		}
+		
+		return e;
+	}
 
-}
+	private List<Employee> selectMulitipleFromEmployeesWithPstmt() {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		List<Employee> emps = new ArrayList<Employee>();
+		try {
+			con =DataBaceConnectionUtil.getConnection();
+			pstmt = con.prepareStatement("select * from employee where empid=? and empname=?" );
+			pstmt.setString(1,"11");
+			pstmt.setString(2,"prasanna");
+			rs = pstmt.executeQuery();
+			while(rs.next()){
+			Employee e = new Employee();
+			String empid = rs.getString("empid");
+			String empname = rs.getString("empName");
+			String desg = rs.getString("designation");
+			e.setEmpid(empid);
+			e.setEmpname(empname);
+			e.setEmpdesg(desg);
+			emps.add(e);
+		
+			}
+			}
+			
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				DataBaceConnectionUtil.closeRs(rs);
+				DataBaceConnectionUtil.closePstmt(pstmt);
+				DataBaceConnectionUtil.closeConnection(con);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return emps;
+	}
+	private void updateWithPrepareStatementOneRow() {
+		Connection con =null;
+		PreparedStatement pstmt=null;
+		try {
+			con = DataBaceConnectionUtil.getConnection();
+			pstmt= con.prepareStatement("update employee set empname=? where empid=?");
+			 pstmt.setString(1,"prasanna");
+			 pstmt.setString(2,"12");
+			 int i = pstmt.executeUpdate();
+			 System.out.println(i  +   "   rows updated");
+		} catch (SQLException e) {
+				e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();	
+		}finally{
+			try {
+				DataBaceConnectionUtil.closePstmt(pstmt);
+				DataBaceConnectionUtil.closeConnection(con);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	private void updateWithPrepareStatementMultipleRows() {
+		Connection con =null;
+		PreparedStatement pstmt=null;
+		try {
+			con = DataBaceConnectionUtil.getConnection();
+			pstmt= con.prepareStatement("update Employee set empname=?,designation=? where  empid=?");
+			 pstmt.setString(1,"srinu");
+			 pstmt.setString(2,"developer");
+			 pstmt.setString(3,"12");
+			 int i = pstmt.executeUpdate();
+			 System.out.println(i  +   "   rows updated");
+		} catch (SQLException e) {
+				e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();	
+		}finally{
+			try {
+				DataBaceConnectionUtil.closePstmt(pstmt);
+				DataBaceConnectionUtil.closeConnection(con);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	private void deleteWithPstmt() {
+		Connection con =null;
+		PreparedStatement pstmt=null;
+		try {
+			con = DataBaceConnectionUtil.getConnection();
+			pstmt= con.prepareStatement("delete from employee where empid=?");
+			 pstmt.setString(1,"11");
+			 int i = pstmt.executeUpdate();
+			 System.out.println(i  +   "   rows deleted");
+		} catch (SQLException e) {
+				e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();	
+		}finally{
+			try {
+				DataBaceConnectionUtil.closePstmt(pstmt);
+				DataBaceConnectionUtil.closeConnection(con);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+		
+	}
+
+	
+
